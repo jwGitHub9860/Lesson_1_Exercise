4a5f27296a6a59115b5b2355786e2467b62a3d9e
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-10-21 17:17:32 -0400

    Corrections for Task L1.1

--- CppND-Program-a-Concurrent-Traffic-Simulation-master/src/TrafficObject.cpp
+++ CppND-Program-a-Concurrent-Traffic-Simulation-master/src/TrafficObject.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <chrono>
-#include <thread>   // needed for ".join()"
+#include <thread>   // defines ".join()"
+#include <algorithm>    // defines "for_each()"
 #include "TrafficObject.h"
+
+using namespace std;
 
 // init static variable
 int TrafficObject::_idCnt = 0;
@@ -27,5 +30,8 @@ TrafficObject::TrafficObject()
 TrafficObject::~TrafficObject()
 {
     // Task L1.1 : Set up a thread barrier that ensures that all the thread objects in the member vector _threads are joined.
-    _threads.join();
+    for_each(threads.begin(), threads.end(), [](thread &t))
+    {
+        t.join();
+    }
 }
